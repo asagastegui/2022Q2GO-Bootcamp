b9df529320f2c53d3b034b715a468f669dd53ce9
Alberto A. Sagastegui Rdz <asagastegui@Alberto-Sagastegui-MacBookPro.local>
2022-05-15 21:38:07 -0500

    Adding entities, controllers, usecases; also finding a new and better way to return json (instead of json.neweconder.encode so you can actually check if the marshall worked before setting a status and responding to client.

---
+++ controllers/controller_pokemons.go
@@ -0,0 +1,51 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"gobootcamp.com/usecases"
+)
+
+// Get all the pokemons (no params required)
+func GetPokemons(w http.ResponseWriter, r *http.Request) {
+	pokemons, err := usecases.ReadCsv("./files/pokemons.csv")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		json.NewEncoder(w).Encode(map[string]string{"error": "There was an error reading the csv file, pls contact the administrator"})
+	}
+	// return all pokemons
+	json.NewEncoder(w).Encode(pokemons)
+}
+
+// Get all pokemons and filter based on the ID of the required pokemon,
+// if id is missing, error 400 will be throwned
+func GetPokemon(w http.ResponseWriter, r *http.Request) {
+	id, IDexists := mux.Vars(r)["id"]
+	if IDexists {
+		pokemons, err := usecases.ReadCsv("./files/pokemons.csv")
+		if err != nil {
+			fmt.Println("Error: ", err)
+			json.NewEncoder(w).Encode(map[string]string{"error": "There was an error reading the csv file, pls contact the administrator"})
+		}
+		// Look for specific pokemon
+		idInt, _ := strconv.Atoi(id)
+		poke, err := usecases.FindPoke(pokemons, idInt)
+		if err != nil {
+			fmt.Println(err)
+			json.NewEncoder(w).Encode(map[string]string{"error": "The pokemon you are looking for doesnt exists"})
+			return
+		}
+		// TODO: Fix to change to use this approach (as its better to check if encoding/marshalling actually worked)
+		// https://stackoverflow.com/questions/31622052/how-to-serve-up-a-json-response-using-go
+		json.NewEncoder(w).Encode(poke)
+		return
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write([]byte("400 - missing ID"))
+	}
+}
--- main.go
+++ main.go
@@ -1,95 +1,20 @@
 package main
 
 import (
-	"encoding/csv"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 
 	"github.com/gorilla/mux"
+	"gobootcamp.com/controllers"
 )
 
 func main() {
 	router := mux.NewRouter()
 
-	router.Path("/pokemons").HandlerFunc(getPokemons).Methods("GET")
-	router.Path("/pokemons/{id:[0-9]+}").HandlerFunc(getPokemons).Methods("GET")
+	router.Path("/pokemons").HandlerFunc(controllers.GetPokemons).Methods("GET")
+	router.Path("/pokemons/{id:[0-9]+}").HandlerFunc(controllers.GetPokemon).Methods("GET")
 
 	fmt.Println("Server at 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
-}
-
-func getPokemons(w http.ResponseWriter, r *http.Request) {
-	pokemons, err := readCsv("./files/pokemons.csv")
-	if err != nil {
-		fmt.Println("Error: ", err)
-		json.NewEncoder(w).Encode(map[string]string{"error": "There was an error reading the csv file, pls contact the administrator"})
-	}
-
-	id, IDexists := mux.Vars(r)["id"]
-	if IDexists {
-		// Look for specific pokemon
-		idInt, _ := strconv.Atoi(id)
-		poke, err := findPoke(pokemons, idInt)
-		if err != nil {
-			fmt.Println(err)
-			json.NewEncoder(w).Encode(map[string]string{"error": "The pokemon you are looking for doesnt exists"})
-			return
-		}
-		json.NewEncoder(w).Encode(poke)
-		return
-	}
-	// return all pokemons
-	json.NewEncoder(w).Encode(pokemons)
-}
-
-func findPoke(pokemons *[]pokemon, id int) (pokemon, error) {
-	for _, pokemon := range *pokemons {
-		if pokemon.ID == id {
-			return pokemon, nil
-		}
-	}
-	return pokemon{}, errors.New("the pokemon you are looking for doesnt exists")
-}
-
-func readCsv(csvPath string) (*[]pokemon, error) {
-	file, err := os.Open(csvPath)
-	if err != nil {
-		return nil, err
-	}
-	// closing the file at the end of readCsv
-	defer file.Close()
-	csvReader := csv.NewReader(file)
-
-	var pokemons []pokemon
-	for {
-		line, err := csvReader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		// Getting ID and Name
-		id, err := strconv.Atoi(line[0])
-		if err != nil {
-			fmt.Println("Unknown ID - ", line)
-			continue
-		}
-		name := line[1]
-		pokemons = append(pokemons, pokemon{ID: id, Name: name})
-	}
-	return &pokemons, nil
-}
-
-// Structs
-type pokemon struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
 }
