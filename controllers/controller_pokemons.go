282be5152529301b7e7d7613f7c4ceebadaf269c
Alberto A. Sagastegui Rdz <asagastegui@Alberto-Sagastegui-MacBookPro.local>
2022-05-24 20:51:48 -0500

    Adding error handling to controller
    Adding function to error handling response writer

--- controllers/controller_pokemons.go
+++ controllers/controller_pokemons.go
@@ -10,45 +10,56 @@ import (
 	"gobootcamp.com/usecases"
 )
 
+func handleError(w http.ResponseWriter, errorStatus int, errorMsg string) {
+	fmt.Println("Error: ", errorMsg)
+	w.WriteHeader(errorStatus)
+	w.Write([]byte(errorMsg))
+}
+
 // GetPokemons - Get all the pokemons (no params required)
 func GetPokemons(w http.ResponseWriter, r *http.Request) {
-	pokemons, err := usecases.ReadCsv("./files/pokemons.csv")
+	pokes, err := usecases.ReadCsv("./files/pokemons.csv")
+	w.Header().Set("Content-Type", "application/json")
 	if err != nil {
-		fmt.Println("Error: ", err)
-		// TODO: Fix to change to use this approach (as its better to check if encoding/marshalling actually worked)
-		// https://stackoverflow.com/questions/31622052/how-to-serve-up-a-json-response-using-go
-		json.NewEncoder(w).Encode(map[string]string{"error": "There was an error reading the csv file, pls contact the administrator"})
+		handleError(w, 500, "There was an error reading the csv file, pls contact the administrator")
+		return
 	}
-	// TODO: Fix to change to use this approach (as its better to check if encoding/marshalling actually worked)
-	// https://stackoverflow.com/questions/31622052/how-to-serve-up-a-json-response-using-go
 	// return all pokemons
-	json.NewEncoder(w).Encode(pokemons)
+	jsonPoke, err := json.Marshal(pokes)
+	if err != nil {
+		handleError(w, 500, "Error converting data to json")
+		return
+	}
+	w.Write(jsonPoke)
 }
 
 // GetPokemon - Get all pokemons and filter based on the ID of the required pokemon, if id is missing, error 400 will be throwned
 func GetPokemon(w http.ResponseWriter, r *http.Request) {
 	id, IDexists := mux.Vars(r)["id"]
+	w.Header().Set("Content-Type", "application/json")
 	if IDexists {
 		pokemons, err := usecases.ReadCsv("./files/pokemons.csv")
 		if err != nil {
-			fmt.Println("Error: ", err)
-			json.NewEncoder(w).Encode(map[string]string{"error": "There was an error reading the csv file, pls contact the administrator"})
+			handleError(w, 500, "There was an error reading the csv file, pls contact the administrator")
+			return
 		}
 		// Look for specific pokemon
 		idInt, _ := strconv.Atoi(id)
 		poke, err := usecases.FindPoke(pokemons, idInt)
 		if err != nil {
-			fmt.Println(err)
-			json.NewEncoder(w).Encode(map[string]string{"error": "The pokemon you are looking for doesnt exists"})
+			handleError(w, 500, "The pokemon you are looking for doesnt exists")
 			return
 		}
-		// TODO: Fix to change to use this approach (as its better to check if encoding/marshalling actually worked)
-		// https://stackoverflow.com/questions/31622052/how-to-serve-up-a-json-response-using-go
-		json.NewEncoder(w).Encode(poke)
+		jsonPoke, err := json.Marshal(poke)
+		if err != nil {
+			handleError(w, 500, "Error converting data to json")
+			return
+		}
+		w.Write(jsonPoke)
 		return
+
 	} else {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(400)
-		w.Write([]byte("400 - missing ID"))
+		handleError(w, 400, "ID parameter required")
+		return
 	}
 }
